9b748a0c859d7f9cd9004b08ab74036366f91aca
Aryan Mahindra <armahin@uw.edu>
2022-04-30 16:02:29 -0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { DEFAULT_ECDH_CURVE } from 'tls';
+import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { Todo } from "./model";
+
+const App: React.FC = () => {
+  const [todo, setToDo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+const handleAdd = (e : React.FormEvent) => {
+  e.preventDefault();
+
+  if(todo) {
+    setTodos([...todos, {id: Date.now(), todo, isDone:false }])
+    setToDo("");
+  }
+
+};
+console.log(todos);
+
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setToDo} handleAdd={handleAdd}/>
+      {<TodoList todos={todos} setTodos={setTodos}/>}
+       
+    
+    </div>
+  );
+}
+
+export default App;
