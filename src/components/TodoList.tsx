a3d1bd90bac3dd56f1ae65faf85bda3df472a6b9
Aryan Mahindra <armahin@uw.edu>
2022-04-30 21:28:00 -0700

    updated

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,19 +9,29 @@ interface Props{
 }
 
 
-const TodoList: React.FC<Props> = ({ todos, setTodos}:Props) => {
-    return <div className='todos'>
-
-        {todos.map(todo => (
-           <SingleTodo 
-           todo={todo} 
-           key={todo.id} 
-           todos={todos}
-           setTodos={setTodos}
-           /> 
-        ))}
-    </div>;
-
-}
+const TodoList: React.FC<Props> = ({ todos, setTodos}) => {
+    return (<div className="container">
+        <div className='todos'>
+            <span className='todos_heading'>
+                Active Tasks
+            </span>
+            {
+                todos.map((todo=>
+                    <SingleTodo todo={todo} todos={todos} key={todo.id} setTodos={setTodos}/>
+                    ))
+            }
+        </div>
+        <div className='todos remove'>
+        <span className='todos_heading'>
+                Completed Tasks
+            </span>
+            {
+                todos.map((todo=>
+                    <SingleTodo todo={todo} todos={todos} key={todo.id} setTodos={setTodos}/>
+                    ))
+            }
+        </div>
+    </div>);
+};
 
 export default TodoList;
